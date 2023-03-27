a47b7199e7d3c89e63563e3895e079e500931037
Fatchul Bari Hikmawan <fb.hikmawan@gmail.com>
2023-03-28 01:09:14 +0700

    fixing vite config dependency

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
